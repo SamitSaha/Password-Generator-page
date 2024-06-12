460ee48a8287e07e7f8f2a42cfe41fcec9bbf39c
Samit Saha <samitsaha99@gmail.com>
2024-06-13 01:36:39 +0600

    generate passsword

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ const length = 12;
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
-const symbols = "~`!@#$%^&*()_+-=[];'\,./{}:|<>?</>";
+const symbol = "~`!@#$%^&*()_+-=[];'\,./{}:|<>?</>";
+
+const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword(){
-    
+    let password = "";
+
+    password += upperCase[Math.floor(math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(math.random() * lowerCase.length)];
+    password += number[Math.floor(math.random() * number.length)];
+    password += symbol[Math.floor(math.random() * symbol.length)];
+    password += allChars[Math.floor(math.random() * allChars.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.ariaSelected();
+    document.execCommand("copy");
 }
